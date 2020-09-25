6b7758931f157271ae33c36f6cdd88ef54cf2272
Bogdan Tudorache <bogdanmihait10@gmail.com>
2020-09-25 15:35:17 +0300

    added layout app and base.html file

--- myshelf/settings.py
+++ myshelf/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'accounts.apps.AccountsConfig',
+    'layout.apps.LayoutConfig',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
