1555d1b01c2bcaa828a42eb06725c86be05b0c8c
Bogdan Tudorache <bogdanmihait10@gmail.com>
2020-09-26 12:43:28 +0300

    added registration functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import (
+    UserRegistrationForm,
+)
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Save the form but don't commmit the action
+            new_user = user_form.save(commit=False)
+            # Take care of the password hashing
+            new_user.set_password(user_form.cleaned_data['password'])
+            # Finally save the form
+            new_user.save()
+            return render(request, 'accounts/user/register_done.html', {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+        return render(request, 'accounts/user/register.html', {'user_form': user_form})
