fb9b855592e95cf55ed987546b2eb3d3a387625f
Bogdan Tudorache <bogdanmihait10@gmail.com>
2020-09-26 13:21:15 +0300

    added crispy-forms and custom css

--- myshelf/settings.py
+++ myshelf/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -121,4 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
