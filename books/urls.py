a4d85024f4d2b1f07ccb51b28d70b6e57ad1084b
Bogdan Tudorache <bogdanmihait10@gmail.com>
2020-09-30 15:32:40 +0300

    added search books by genre and modified search template layout

--- books/urls.py
+++ books/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path, include
+from django.urls import path
 
 from . import views
 
 
 urlpatterns = [
-    path('search/all_books/', views.book_search_list, name='book_list'),
+    path('search/all/', views.book_search_list, name='book_list'),
+    path('search/<int:genre_id>/', views.book_search_list, name='book_list_by_genre'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 
-from .models import Book
+from .models import Book, Genre
 
 
-def book_search_list(request):
-    books = Book.objects.all()
-    return render(request, 'books/book_search_list.html', {'books': books, 'section': 'search'})
+def book_search_list(request, genre_id=None):
+    if genre_id is None:
+        books = Book.objects.all()
+    else:
+        books = Book.objects.filter(genre__id=genre_id)
+    genres = Genre.objects.all()
+    return render(request, 'books/book_search_list.html', {'books': books,
+                                                           'section': 'search',
+                                                           'genres': genres})
