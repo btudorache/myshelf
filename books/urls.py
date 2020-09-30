e7735350a70a0eae58e199b93d1053dd87ea2840
Bogdan Tudorache <bogdanmihait10@gmail.com>
2020-09-30 21:56:37 +0300

    added book detail view and template

--- books/urls.py
+++ books/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path('search/all/', views.book_search_list, name='book_list'),
-    path('search/<int:genre_id>/', views.book_search_list, name='book_list_by_genre'),
+    path('search/genre/<int:genre_id>/', views.book_search_list, name='book_list_by_genre'),
+    path('search/<int:book_id>/', views.book_detail, name='book_detail'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Book, Genre
 
@@ -12,3 +12,8 @@ def book_search_list(request, genre_id=None):
     return render(request, 'books/book_search_list.html', {'books': books,
                                                            'section': 'search',
                                                            'genres': genres})
+
+
+def book_detail(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    return render(request, 'books/book_detail.html', {'book': book, 'section': 'book_detail'})
