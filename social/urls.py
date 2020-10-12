43691ac53d2280ac634a62a640fc04d0ebc492b5
Bogdan Tudorache <bogdanmihait10@gmail.com>
2020-10-12 14:37:13 +0300

    added basic views and templates for user_list and user_detail

--- social/urls.py
+++ social/urls.py
@@ -5,4 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('social/', views.user_list, name='user_list'),
+    path('social/<int:user_id>', views.user_detail, name='social_user_detail'),
 ]
--- social/views.py
+++ social/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import render, get_object_or_404
 
 
 def home(request):
     return render(request, 'social/home.html', {'section': 'home'})
+
+
+def user_list(request):
+    users = User.objects.exclude(username=request.user.username)
+    return render(request, 'social/user_list.html', {'section': 'social',
+                                                     'users': users,})
+
+
+def user_detail(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+    return render(request, 'social/user_detail.html', {'section': 'social',
+                                                       'user': user,})
