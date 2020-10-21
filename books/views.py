1e05f5d406ad631140722aedde4f8ffaeff9ca63
Bogdan Tudorache <bogdanmihait10@gmail.com>
2020-10-21 18:41:18 +0300

    added paginator for books and reviews

--- books/views.py
+++ books/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
@@ -15,12 +16,13 @@ from .forms import BookRateForm, SearchForm
 @login_required
 def book_search_list(request, genre_id=None, author_id=None):
     search_form = SearchForm()
+    genres = Genre.objects.all()
     if genre_id:
         books = Book.objects.filter(genre__id=genre_id)
     # Form variable 'query' is given to the request dictionary if the form on the website is submitted
     elif author_id:
         books = Book.objects.filter(author__id=author_id)
-    elif 'query' in request.GET:
+    elif 'query' in request.GET and request.GET['query']:
         search_form = SearchForm(data=request.GET)
         if search_form.is_valid():
             search_object = search_form.cleaned_data['query']
@@ -30,13 +32,17 @@ def book_search_list(request, genre_id=None, author_id=None):
             else:
                 books = Book.objects.filter(Book.create_search_query(search_object.split()))
     else:
-        search_form = SearchForm()
         books = Book.objects.all()
-    genres = Genre.objects.all()
-    return render(request, 'books/book_search_list.html', {'books': books,
-                                                           'section': 'search',
+
+
+    paginator = Paginator(books, 1)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'books/book_search_list.html', {'section': 'search',
                                                            'genres': genres,
-                                                           'search_form': search_form})
+                                                           'search_form': search_form,
+                                                           'page_obj': page_obj})
 
 
 @login_required
@@ -45,13 +51,17 @@ def book_detail(request, book_id):
     book = get_object_or_404(Book, id=book_id)
     book_rate_form = BookRateForm()
     rating = BookRating.get_book_rating(request.user, book)
-    reviews = BookReview.objects.filter(book_reviewed=book)[:5]
+    reviews = BookReview.objects.filter(book_reviewed=book)
+    paginator = Paginator(reviews, 8)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
     return render(request, 'books/book_detail.html', {'book': book,
                                                       'section': 'search',
                                                       'book_rate_form': book_rate_form,
                                                       'rating': rating,
-                                                      'reviews': reviews})
+                                                      'page_obj': page_obj})
 
 
 @require_POST
