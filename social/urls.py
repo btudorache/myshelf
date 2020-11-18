283694c8d15d28b08cf545f63811df3877fe1159
Bogdan Tudorache <bogdanmihait10@gmail.com>
2020-11-18 20:11:50 +0200

    changed url path

--- social/urls.py
+++ social/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('social/', views.user_list, name='user_list'),
     path('social/users/follow/<int:user_id>/', views.user_follow, name='user_follow'),
     path('social/users/unfollow/<int:user_id>/<int:contact_id>/', views.user_follow, name='user_unfollow'),
-    path('social/<int:user_id>', views.user_detail, name='social_user_detail'),
+    path('social/<int:user_id>', views.user_detail, name='user_detail'),
 ]
