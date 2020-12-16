af8e3eb7168f12e1a637fb18bbbf69553fed90b8
Bogdan Tudorache <bogdanmihait10@gmail.com>
2020-12-16 20:36:59 +0200

    fixed integrity and other issues for initial dataset (to be able to add functional tests on the existing initial dataset)

--- functional_tests/test_registration.py
+++ functional_tests/test_registration.py
@@ -2,8 +2,22 @@ from .base import FunctionalTest
 
 
 class CreateAccountTest(FunctionalTest):
+    def test_login(self):
+        self.browser.get(self.live_server_url)
+
+        # go to login page
+        self.browser.find_element_by_id("login-button").click()
+
+        # fill in login data
+        self.browser.find_element_by_id("id_username").send_keys("opak")
+        self.browser.find_element_by_id("id_password").send_keys("test")
+
+        # login account
+        self.browser.find_element_by_css_selector("input.btn.btn-primary").click()
+
     def test_create_account(self):
         self.browser.get(self.live_server_url)
+
         # go to register page
         self.browser.find_element_by_id("registration-button").click()
 
@@ -18,7 +32,7 @@ class CreateAccountTest(FunctionalTest):
         # go to login page
         self.browser.find_element_by_id("login-link").click()
 
-        # fill in log in data
+        # fill in login data
         self.browser.find_element_by_id("id_username").send_keys("test_user10")
         self.browser.find_element_by_id("id_password").send_keys("testing1234")
 
